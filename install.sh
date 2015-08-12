4343d4dd6ec6ba49ea1c8053f042d33ba8856b6a
Thomas Coats <github@voodoolabs.net>
2015-08-12 23:12:21 +1200

    Simplification

--- install.sh
+++ install.sh
@@ -3,13 +3,9 @@ set -e
 
 # install runit
 cp -R /install/runit/* /
-mkdir /etc/service
 
 # install syslog-ng
-apk update
-apk add syslog-ng
-rm -rf /var/lib/apt/lists/*
-#mkdir /var/run/syslog-ng
+apk-install syslog-ng
 cp -R /install/syslog-ng/* /
 
 # remove install dir
